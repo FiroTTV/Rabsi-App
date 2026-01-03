f2f1480dec8319459aa7c9d9a979585b0b0589a3
FiroTTV <finn.rogozinskizwei@gmail.com>
2026-01-03 14:18:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = "rabsi-streak-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow("./")
   );
 });
